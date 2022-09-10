99b274abf3a1debe94919a9fa8202edf89358c03
Alvinci Merquita <alvinci.merquita@gmail.com>
2022-09-10 17:17:40 +0800

    update mongodb.js with search examples

--- mongodb.js
+++ mongodb.js
@@ -1,59 +1,49 @@
-// const mongodb = require('mongodb')
-// const MongoClient = mongodb.MongoClient
-// const ObjectID = mongodb.ObjectId
-
 const { MongoClient, ObjectID, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-account'
 
-// const id = new ObjectId()
-// console.log(id)
-// console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) { return console.log('Unable to connect to database!') }  
     console.log('Connected correctly!')
-
+    
     const db = client.db(databaseName)
 
-    // Insert One
-    // db.collection('users').insertOne({
-    //     name: 'Bob',
-    //     age: 29
-    // }, (error, result) => {
-    //     if (error) { return console.log('Unable to insert user') }
-    //     console.log(result.insertedId)
+    // Search One by content or ID
+    // db.collection('users').findOne( { name: 'Alice', age: 28}, (error, user) =>{
+    //     if (error) { return console.log('Unable to fetch!') }  
+    //     console.log(user)
     // })
-
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Scarlet',
-    //     age: 26
-    // }, (error, result) => {
-    //     if (error) { return console.log('Unable to insert user') }
-    //     console.log(result.insertedId)
+    // db.collection('users').findOne( { _id: ObjectId("631c4a379aff45918542379e") }, (error, user) =>{
+    //     if (error) { return console.log('Unable to fetch!') }  
+    //     console.log(user)
     // })
     
-    // Insert Many
-    // db.collection('users').insertMany([
-    //     { name: 'Grace', age: 34 },
-    //     { name: 'Dorothy', age: 39 }
-    // ],(error, result)=>{
-    //     if (error) { return console.log('Unable to insert documents') }
-    //     console.log(result.insertedIds)
+    // Get users with age = 26
+    // db.collection('users').find({ age: 26 }).toArray((error, users) => {
+        //     console.log(users)
+        // })
+        
+    // Count users with age = 26
+    // db.collection('users').countDocuments({ age: 26 },(error, count) => {
+        //     console.log(count)
     // })
     
-    // Example
-    // db.collection('tasks').insertMany([
-    //     { description: 'Distance Run', completed: false },
-    //     { description: 'Eat Breakfast', completed: true },
-    //     { description: 'Meet Parents', completed: true },
-    // ],(error, result)=>{
-    //     if (error) { return console.log('Unable to insert documents') }
-    //     console.log(result.insertedIds)
+    // Find users with age > 30
+    // db.collection('users').find({ age: { $gt: 30 } }).toArray((error,users) => {
+    //     console.log(users)
     // })
-
+    
+    // Find users with age = {x : x is [27,30]}
+    // db.collection('users').find(
+    //     { $and: [
+    //         { age : {$gte: 27} },
+    //         { age : {$lte: 30} },
+    //     ] 
+    //     } )
+    // .toArray((error,users) => {
+    //     console.log(users)
+    // })
 
 })
 
