01020f75903c08da6ee549cdb19102f3e61fcdfb
Alvinci Merquita <alvinci.merquita@gmail.com>
2022-09-10 17:50:23 +0800

    update mondodb.js with delete examples

--- mongodb.js
+++ mongodb.js
@@ -9,36 +9,24 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     
     const db = client.db(databaseName)
 
-    // Update One
-    // db.collection('users').updateOne({
-    //     _id: new ObjectId("631c43712fbb8748221f6342")
-    // }, {
-    //     // set age to 26
-    //     // $set: { age: 26 }
-
-    //     // increment age ( positive or negative )
-    //     $inc : { age: -1 }
-
-    // }).then((result) => {
+    // Delete Many
+    // db.collection('users').deleteMany({
+    //     age: 26
+    // }).then((result)=>{
     //     console.log(result)
-    // }).catch( (error) => {
+    // }).catch((error)=>{
     //     console.log(error)
     // })
-
-    // Update Many: Update status in all tasks to true
-    // db.collection('tasks').updateMany({
-    //     // find those tasks with false
-    //     completed: false
-    // }, {
-    //     // operations we want to perform for those found data
-    //     $set: { completed: true }
-    // }).then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-
-    // })
-
+    
+    // Delete One
+    db.collection('tasks').deleteOne({
+        description: "Search Pen"
+    }).then((result)=>{
+        console.log(result)
+    }).catch((error)=>{
+        console.log(error)
+    })
+    
 })
 
 
