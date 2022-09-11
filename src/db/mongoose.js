4358186f3a665381be2cb39e9379b99f45f48518
Alvinci Merquita <alvinci.merquita@gmail.com>
2022-09-11 21:25:12 +0800

    feat post request example and some refactoring

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,74 +3,7 @@ const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-account-api')
 
-// User and Task model with validation and sanitization
-const User = mongoose.model('User', {
-    name: { 
-        type: String, 
-        required: true,
-        trim: true
-    },
-    email:{
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if (!validator.isEmail(value)){ throw new Error('Email is invalid') }
-        }
-    },
-    password:{
-        type: String,
-        required: true,
-        minlength: 7,
-        trim: true,
-        validate(value){
-            if (value.toLowerCase().includes('password')){ throw new Error('Password cannot contain "password"')}
-        }
-    },
-    age: { 
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0){ throw new Error('Age must be a positive number') }
-        }
-    }
-
-})
-
-const Task = mongoose.model('Task', {
-    description: { 
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: { 
-        type: Boolean,
-        default: false
-    }
-})
 
 
-// const me = new User({
-//     name: '    Michael  ',
-//     email: 'MICHAEL@mail.com     ',
-//     password: '  re23456   ',
-//     age: -1
-// })
 
-// me.save().then(()=>{
-//     console.log(me)
-// }).catch((error)=>{ 
-//     console.log(error)
-// })
-
-// const task = new Task({
-//     description: 'Eat Lunch    '
-// })
-
-// task.save().then(()=>{
-//     console.log(task)
-// }).catch((error)=>{ 
-//     console.log('Error!',error)
-// })
 
