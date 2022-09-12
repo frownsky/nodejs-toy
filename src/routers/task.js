f3c45e35b21a95434b5c119bb1feb38f56acdef0
Alvinci Merquita <alvinci.merquita@gmail.com>
2022-09-12 17:34:36 +0800

    refactor index.js and routing stuff

---
+++ src/routers/task.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = express.Router()
+
+router.post('/', async (req, res)=>{
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(error){
+        res.status(400).send(error)
+    }
+})
+
+router.get('/', async (req, res) => {
+    try {
+        const task = await Task.find({})
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if (!task) { return res.status(404).send() }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.patch('/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description','completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update) )
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!'})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id,  req.body, { new: true, runValidators: true } )
+        if (!task) { 
+            res.status(404).send() 
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task){
+            res.status(404).send() 
+        }
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
