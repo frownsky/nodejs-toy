494920d044b3583d7c0ef87af8876df118f38fc8
Alvinci Merquita <alvinci.merquita@gmail.com>
2022-09-20 20:19:17 +0800

    refactor auth in update, delete routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -57,21 +57,7 @@ router.get('/me', auth, async (req, res) => {
 })
 
 // protected route
-router.get('/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user){
-            res.status(404).send()
-        }
-        res.send(user)
-    } catch (error) {
-        res.status(500).send(error)
-    }  
-})
-
-// protected route
-router.patch('/:id', async (req, res) => {
+router.patch('/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name','email','password','age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update) )
@@ -79,27 +65,19 @@ router.patch('/:id', async (req, res) => {
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!'})
     }
-    
     try {
-        const user = await User.findById(req.params.id)
-        if (!user) { 
-            res.status(404).send() 
-        }
-        updates.forEach((update) => user[update] = req.body[update] )
-        await user.save()
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update] )
+        await req.user.save()
+        res.send(req.user)
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
-router.delete('/:id', async (req, res) => {
+router.delete('/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user){
-            res.status(404).send() 
-        }
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (error) {
         res.status(500).send(error)
     }
